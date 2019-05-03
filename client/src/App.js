f7a991e175930916b6fa557b3044e7500bc6cd5c
SOIP <38543876+sumofitsparts@users.noreply.github.com>
2019-05-03 16:53:53 -0400

    added react-router-dom and paypal-rest-sdk

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,64 @@
 import React from 'react';
 import logo from './logo.svg';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import './App.css';
 
-function App() {
+const Home = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          React PayPal App
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>HOME</h1>
+      <a href="/purchase">BUY</a>
     </div>
-  );
+  )
+}
+
+class Purchase extends React.Component {
+
+    componentDidMount() {
+      fetch('/buy') 
+      .then(function(res) {
+        console.log(res)
+        window.location=res.url;
+      })
+      .catch(error => console.log(error) );
+    }
+
+    render() {
+      return (
+        <div>
+          <h1>Buy</h1>
+        </div>
+      )
+    }
+}
+
+const Success = () => {
+  return (
+    <div>
+      <h1>Success</h1>
+    </div>
+  )
+}
+
+const Err = () => {
+  return (
+    <div>
+      <h1>Error</h1>
+    </div>
+  )
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Route path="/" component={Home} exact />
+        <Route path="/purchase" component={Purchase}/>
+        <Route path="/success" component={Success}/>
+        <Route path="/err" component={Err}/>
+      </BrowserRouter>
+    );
+  } 
 }
 
 export default App;
--- server.js
+++ server.js
@@ -1,11 +1,76 @@
 const express = require('express')
 const app = express()
 const port = 5000
+let paypal = require('paypal-rest-sdk');
+let ID = process.env.ID;
+let secret = process.env.secret;
+
+//start nodejs and react servers using npm run dev
 
 app.get("/", function(req, res){
     res.sendFile('client/public' + "index.html");
 })
 
-// app.use('/static', express.static(path.join(__dirname, 'client/public')))
+app.get('/buy' , ( req , res ) => {
+        
+    // var ammount = req.body.price;
+
+    paypal.configure({
+        'mode': 'sandbox',
+        'client_id': ID,
+        'client_secret': secret
+      });
+
+    var create_payment_json = {
+        "intent": "authorize",
+        "payer": {
+            "payment_method": "paypal"
+        },
+        "redirect_urls": {
+            "return_url": "/success",
+            "cancel_url": "/err"
+        },
+        "transactions": [{
+            "item_list": {
+                "items": [{
+                    "name": "item",
+                    "sku": "item",
+                    "price": "1.00",
+                    "currency": "USD",
+                    "quantity": 1
+                }]
+            },
+            "amount": {
+                "currency": "USD",
+                "total": "1.00"
+            },
+            "description": "This is the payment description."
+        }]
+    };
+
+    paypal.payment.create(create_payment_json, function (error, payment) {
+        console.log("works")
+        if (error) {
+            throw error;
+            res.redirect('/err');
+        } else {
+            console.log("Create Payment Response");
+            console.log(payment);
+            var url = payment.links[1].href;
+            return res.redirect(url);
+            console.log(url)
+        }
+    });
+});
+
+app.get('/success' , (req ,res ) => {
+    console.log(req.query); 
+    res.send('success'); 
+})
+
+app.get('/err' , (req , res) => {
+    console.log(req.query); 
+    res.send('error'); 
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
