a7cbe57e2e783bb7305619a163b552e044f2bb94
SOIP <38543876+sumofitsparts@users.noreply.github.com>
2019-05-04 13:50:30 -0400

    heroku set up

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 let paypal = require('paypal-rest-sdk');
 let ID = process.env.ID;
 let secret = process.env.secret;
